ea6d729bcdad20415d8dbf022d364872fe26b8d8
Ola Tranum Arnegaard <olatar@stud.ntnu.no>
2019-04-21 23:40:21 +0700

    Done dox (again)

---
+++ rammeverk/html/search/all_2.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['fsm_2ec',['fsm.c',['../fsm_8c.html',1,'']]],
+  ['fsm_2eh',['fsm.h',['../fsm_8h.html',1,'']]],
+  ['fsm_5fbetween_5ffloors',['FSM_between_floors',['../fsm_8h.html#a39a26887999c356bdd9e6c987216de10',1,'fsm.h']]],
+  ['fsm_5fcurrent_5ffloor',['FSM_current_floor',['../fsm_8h.html#aa6af10e180dc4d2a022b552bc35f254a',1,'fsm.h']]],
+  ['fsm_5fcurrent_5fstate',['FSM_current_state',['../fsm_8h.html#a7b1733bd3a359809da72fff865667ecc',1,'fsm.h']]],
+  ['fsm_5fdecide_5fdirection',['FSM_decide_direction',['../fsm_8c.html#a9f1971a841faf147ccc8ad6df9f121bc',1,'FSM_decide_direction():&#160;fsm.c'],['../fsm_8h.html#a9f1971a841faf147ccc8ad6df9f121bc',1,'FSM_decide_direction():&#160;fsm.c']]],
+  ['fsm_5fdesired_5ffloor',['FSM_desired_floor',['../fsm_8h.html#abaad92b5fd85de2684c0c3a61a6613fd',1,'fsm.h']]],
+  ['fsm_5fevent_5fbutton',['FSM_event_button',['../fsm_8c.html#ada5cb6b2205c32c01818ed7cdd4a8996',1,'FSM_event_button(elev_button_type_t button, int floor):&#160;fsm.c'],['../fsm_8h.html#ada5cb6b2205c32c01818ed7cdd4a8996',1,'FSM_event_button(elev_button_type_t button, int floor):&#160;fsm.c']]],
+  ['fsm_5fevent_5ffloor',['FSM_event_floor',['../fsm_8c.html#a772891aa3ea0220c5aeaec6817feb1a5',1,'FSM_event_floor(int floor):&#160;fsm.c'],['../fsm_8h.html#a772891aa3ea0220c5aeaec6817feb1a5',1,'FSM_event_floor(int floor):&#160;fsm.c']]],
+  ['fsm_5finit',['FSM_init',['../fsm_8c.html#a4bc68d2016b4febdd4e10280d44fc00a',1,'FSM_init():&#160;fsm.c'],['../fsm_8h.html#a4bc68d2016b4febdd4e10280d44fc00a',1,'FSM_init():&#160;fsm.c']]],
+  ['fsm_5fq_5fempty',['FSM_q_empty',['../fsm_8h.html#a35a3b6464dac9a715d4cca0e9031473c',1,'fsm.h']]],
+  ['fsm_5fset_5fstate',['FSM_set_state',['../fsm_8c.html#a807a4ecd4cbdf49773138bcd62f80a85',1,'FSM_set_state(state s):&#160;fsm.c'],['../fsm_8h.html#a807a4ecd4cbdf49773138bcd62f80a85',1,'FSM_set_state(state s):&#160;fsm.c']]],
+  ['fsm_5fstop_5fdepressed_5flogic',['FSM_stop_depressed_logic',['../fsm_8c.html#a51dc03c6aadcb22bbeb0cf6112395a94',1,'FSM_stop_depressed_logic():&#160;fsm.c'],['../fsm_8h.html#a51dc03c6aadcb22bbeb0cf6112395a94',1,'FSM_stop_depressed_logic():&#160;fsm.c']]],
+  ['fsm_5fstop_5fpressed',['FSM_stop_pressed',['../fsm_8h.html#a9ffc63e30932c82193c3df939a544147',1,'fsm.h']]],
+  ['fsm_5fstop_5fpressed_5flogic',['FSM_stop_pressed_logic',['../fsm_8c.html#aa5f2316d69c6e3e7bbfd0328d4cb2e7f',1,'FSM_stop_pressed_logic():&#160;fsm.c'],['../fsm_8h.html#aa5f2316d69c6e3e7bbfd0328d4cb2e7f',1,'FSM_stop_pressed_logic():&#160;fsm.c']]]
+];
---
+++ rammeverk/html/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "cefimnqst",
+  1: "cefimqt",
+  2: "efimqt",
+  3: "fq",
+  4: "e",
+  5: "st",
+  6: "n"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "variables",
+  4: "typedefs",
+  5: "enums",
+  6: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Variables",
+  4: "Typedefs",
+  5: "Enumerations",
+  6: "Macros"
+};
+
